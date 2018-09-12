45da1775b782c146fcf27bcc8bd480902c175994
yangchong211 <yangchong211@163.com>
2018-09-12 17:45:48 +0800

    修改适配方案

--- app/src/ceshi/java/cn/ycbjie/ycaudioplayer/Config.java
+++
@@ -1,9 +0,0 @@
-package cn.ycbjie.ycaudioplayer;
-
-
-public class Config {
-    /**
-     * https://blog.csdn.net/qq_30878303/article/details/79536921
-     */
-    public static final String hjText = "测试环境";
-}
--- app/src/shengchan/java/cn.ycbjie.ycaudioplayer/Config.java
+++
@@ -1,5 +0,0 @@
-package cn.ycbjie.ycaudioplayer;
-
-public class Config {
-    public static final String hjText = "生产环境";
-}
